f0ffe1fd62f269f7fc694de946eafd51ff7c242c
Saad <mail@saad.codes>
2022-10-12 15:12:33 -0500

    Updated code

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -53,6 +53,11 @@ router.get("/", auth.optional, function(req, res, next) {
     query.tagList = { $in: [req.query.tag] };
   }
 
+  if (typeof req.query.title !== "undefined") {
+    query.title = { $in: [req.query.title] };
+  }
+
+
   Promise.all([
     req.query.seller ? User.findOne({ username: req.query.seller }) : null,
     req.query.favorited ? User.findOne({ username: req.query.favorited }) : null
